35b85707cef1b78cb9dec7853c3b12184bfca33f
devlopper <kycdev@gmail.com>
2022-01-25 13:57:19 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/collectif/server/client/rest/LegislativeActVersion.java
+++ src/main/java/ci/gouv/dgbf/system/collectif/server/client/rest/LegislativeActVersion.java
@@ -19,9 +19,17 @@ public class LegislativeActVersion extends AbstractObject implements Serializabl
 	@JsonbProperty(value = LegislativeActVersionDto.JSON_NAME) private String name;
 	@JsonbProperty(value = LegislativeActVersionDto.JSON_BUDGETARY_ACT) private LegislativeAct act;
 	@JsonbProperty(value = LegislativeActVersionDto.JSON_BUDGETARY_ACT_IDENTIFIER) private String actIdentifier;
+	@JsonbProperty(value = LegislativeActVersionDto.JSON___AUDIT__) String __audit__;
 	
 	@Override
 	public String toString() {
 		return name;
 	}
+	
+	public static final String FIELD_IDENTIFIER = "identifier";
+	public static final String FIELD_CODE = "code";
+	public static final String FIELD_NAME = "name";
+	public static final String FIELD_ACT = "act";
+	public static final String FIELD_ACT_IDENTIFIER = "actIdentifier";
+	public static final String FIELD___AUDIT__ = "__audit__";
 }
